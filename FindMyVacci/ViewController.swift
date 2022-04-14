34a46fa0761bf1ff3b1ff4660c39002297128114
rvancraenenbroek <robin.van.craenenbroek@student.ehb.be>
2022-04-14 16:21:00 +0200

    Re-initializing project setup

---
+++ FindMyVacci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindMyVacci
+//
+//  Created by Huei Li Yap on 14/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
