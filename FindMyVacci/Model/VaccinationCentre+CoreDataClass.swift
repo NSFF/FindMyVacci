6f20b42f35f11d12a18218b6707cacf37cd200ef
rvancraenenbroek <robin.van.craenenbroek@student.ehb.be>
2022-04-26 20:28:25 +0200

    test

---
+++ FindMyVacci/Model/VaccinationCentre+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  VaccinationCentre+CoreDataClass.swift
+//  FindMyVacci
+//
+//  Created by Huei Li Yap on 26/04/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(VaccinationCentre)
+public class VaccinationCentre: NSManagedObject {
+//
+}
