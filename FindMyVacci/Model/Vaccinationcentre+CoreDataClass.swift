ee8e007f77b581161d6e70fe41fb98a2bd5e8fc0
rvancraenenbroek <robin.van.craenenbroek@student.ehb.be>
2022-04-26 20:04:23 +0200

    Changed entity model to english + codegen of the entity model to extension + Added model folder with Vaccinationcentre class

---
+++ FindMyVacci/Model/Vaccinationcentre+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Vaccinationcentre+CoreDataClass.swift
+//  FindMyVacci
+//
+//  Created by Huei Li Yap on 26/04/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Vaccinationcentre)
+public class Vaccinationcentre: NSManagedObject {
+
+}
