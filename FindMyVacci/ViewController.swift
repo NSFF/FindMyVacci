ec81c9a5d003cea850f3daf8af308af19684396c
rvancraenenbroek <robin.van.craenenbroek@student.ehb.be>
2022-04-26 20:54:23 +0200

    Added data retrieval from core data context in view controller and a function to fetch all vaccination centres in view controller (which still needs to be implemented)

--- FindMyVacci/ViewController.swift
+++ FindMyVacci/ViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Reference to the managed object context (= the core data container)
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+    // Data for the TableView
+    var items:[VaccinationCentre]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        fetchVacci()
     }
+    
+    func fetchVacci(){
+        // Fetch data from core data
+    }
+
 
 
 }
