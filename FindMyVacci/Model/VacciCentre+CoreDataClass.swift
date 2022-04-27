2afc872f0ad3a5fd4e7d0dc090a5564b3f094850
rvancraenenbroek <robin.van.craenenbroek@student.ehb.be>
2022-04-27 20:35:37 +0200

    tested default setup of maps + added default images for vaccination centres

---
+++ FindMyVacci/Model/VacciCentre+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  VacciCentre+CoreDataClass.swift
+//  FindMyVacci
+//
+//  Created by Huei Li Yap on 27/04/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(VacciCentre)
+public class VacciCentre: NSManagedObject {
+
+}
--- FindMyVacci/ViewController.swift
+++ FindMyVacci/ViewController.swift
@@ -4,16 +4,19 @@
 //
 //  Created by Huei Li Yap on 14/04/2022.
 //
-
+import CoreLocation
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     // Reference to the managed object context (= the core data container)
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
     // Data for the TableView
     var items:[VaccinationCentre]?
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +26,20 @@ class ViewController: UIViewController {
         // tableView.delegate = self
         
         fetchVacci()
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.startUpdatingLocation()
+        }
     }
+
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        var coordinate = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        var region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        mapView.setRegion(region, animated: true)
+    }
+    
     
     func fetchVacci(){
         // Fetch data from core data to display in Table View
